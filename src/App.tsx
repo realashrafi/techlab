e6d96d0e901f04bf168a31997cfd8e8053e55f5c
ali <zboom0ne01@gmail.com>
2025-08-02 11:54:24 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 // @ts-ignore
 import { BrowserMultiFormatReader, Result } from '@zxing/library/esm/browser/BrowserMultiFormatReader';
 import { motion, AnimatePresence } from 'framer-motion';
+import {database} from "./components/database";
 
 // تعریف تایپ‌ها
 interface BarcodeData {
@@ -64,148 +65,7 @@ const BarcodeScanner: React.FC = () => {
   ];
 
   // دیتابیس فیک
-  const fakeBarcodeData: BarcodeData[] = [
-    {
-      barcode: "PAST987654321",
-      label: "CE3",
-      address_receiver: "No. 12, Valiasr St., District 1, Tehran, Iran",
-      address_sender: "Apt. 4, Noor St., Mashhad, Iran",
-      next_yar: "Narges Shafiei"
-    },
-    {
-      barcode: "PAST987654323",
-      label: "CS2",
-      address_receiver: "Apt. 5, Enghelab St., District 2, Tehran, Iran",
-      address_sender: "No. 8, Persian Gulf Blvd., Bandar Abbas, Iran",
-      next_yar: "Zahra Mohammadi"
-    },
-    {
-      barcode: "PAST987654325",
-      label: "T3",
-      address_receiver: "Building 7, Babolsar Coastal Rd., Babolsar, Iran",
-      address_sender: "Unit 7, Shariati Ave., District 3, Tehran, Iran",
-      next_yar: "Maryam Amini"
-    },
-    {
-      barcode: "PAST987654327",
-      label: "CS1",
-      address_receiver: "Building 8, Saadat Abad, District 5, Tehran, Iran",
-      address_sender: "Unit 5, Zand St., Shiraz, Iran",
-      next_yar: "Sara Ranjbar"
-    },
-    {
-      barcode: "PAST987654329",
-      label: "CN1",
-      address_receiver: "No. 9, Vali-Asr St., District 6, Tehran, Iran",
-      address_sender: "Apt. 3, Rasht Central St., Rasht, Iran",
-      next_yar: "Elnaz Keshavarz"
-    },
-    {
-      barcode: "PAST987654331",
-      label: "CW1",
-      address_receiver: "Apt. 4, Niavaran Blvd., District 7, Tehran, Iran",
-      address_sender: "No. 12, Shahrdari St., Urmia, Iran",
-      next_yar: "Mahsa Rahimi"
-    },
-    {
-      barcode: "PAST987654333",
-      label: "CW2",
-      address_receiver: "Unit 2, Azadi Sq., District 9, Tehran, Iran",
-      address_sender: "Apt. 1, Kermanshah Blvd., Kermanshah, Iran",
-      next_yar: "Reyhaneh Ghaffari"
-    },
-    {
-      barcode: "PAST987654335",
-      label: "CE1",
-      address_receiver: "No. 15, Pasdaran St., District 10, Tehran, Iran",
-      address_sender: "Unit 11, Bojnord St., Bojnord, Iran",
-      next_yar: "Nazanin Khademi"
-    },
-    {
-      barcode: "PAST987654337",
-      label: "CW4",
-      address_receiver: "Building 3, Jomhouri Ave., District 12, Tehran, Iran",
-      address_sender: "No. 6, Zanjan Main St., Zanjan, Iran",
-      next_yar: "Fatemeh Aslani"
-    },
-    {
-      barcode: "PAST987654339",
-      label: "CC1",
-      address_receiver: "Apt. 11, Shahrak Gharb, District 13, Tehran, Iran",
-      address_sender: "Apt. 9, Fahadan St., Yazd, Iran",
-      next_yar: "Taraneh Sadat"
-    },
-    {
-      barcode: "PAST987654341",
-      label: "CW3",
-      address_receiver: "No. 6, Motahari St., District 14, Tehran, Iran",
-      address_sender: "Apt. 14, Farhang Blvd., Tabriz, Iran",
-      next_yar: "Amir Karimi"
-    },
-    {
-      barcode: "PAST987654343",
-      label: "CS2",
-      address_receiver: "Unit 9, Darband St., District 15, Tehran, Iran",
-      address_sender: "Building 9, Khoramshahr St., Ahvaz, Iran",
-      next_yar: "Reza Jafari"
-    },
-    {
-      barcode: "PAST987654345",
-      label: "CS6",
-      address_receiver: "Building 7, Ekbatan Town, District 16, Tehran, Iran",
-      address_sender: "Unit 4, Qeshm Island, Iran",
-      next_yar: "Ali Tabrizi"
-    },
-    {
-      barcode: "PAST987654347",
-      label: "CS5",
-      address_receiver: "Apt. 10, Hafez St., District 17, Tehran, Iran",
-      address_sender: "No. 7, Imam Khomeini Blvd., Kerman, Iran",
-      next_yar: "Saeed Moradi"
-    },
-    {
-      barcode: "PAST987654349",
-      label: "CW3",
-      address_receiver: "No. 8, Yadegar-e Emam Blvd., District 18, Tehran, Iran",
-      address_sender: "Building 12, Tabriz, Iran",
-      next_yar: "Mohammad Afshar"
-    },
-    {
-      barcode: "PAST987654351",
-      label: "CS1",
-      address_receiver: "Unit 5, Lavizan St., District 19, Tehran, Iran",
-      address_sender: "Apt. 3, Shiraz, Iran",
-      next_yar: "Mehdi Vaziri"
-    },
-    {
-      barcode: "PAST987654353",
-      label: "CE3",
-      address_receiver: "Building 4, Rey Blvd., District 20, Tehran, Iran",
-      address_sender: "No. 15, Mashhad, Iran",
-      next_yar: "Hossein Danesh"
-    },
-    {
-      barcode: "PAST987654355",
-      label: "T4",
-      address_receiver: "Unit 6, Rasht, Iran",
-      address_sender: "Apt. 6, Narmak, District 21, Tehran, Iran",
-      next_yar: "Pouria Ghodrati"
-    },
-    {
-      barcode: "PAST987654357",
-      label: "T14",
-      address_receiver: "No. 10, Urmia, Iran",
-      address_sender: "No. 14, Piroozi St., District 22, Tehran, Iran",
-      next_yar: "Keyvan Soltani"
-    },
-    {
-      barcode: "PAST987654359",
-      label: "CE1",
-      address_receiver: "Unit 3, Sadeghieh, District 1, Tehran, Iran",
-      address_sender: "Apt. 2, Bojnord, Iran",
-      next_yar: "Nima Esmaili"
-    }
-  ];
+  const fakeBarcodeData: BarcodeData[] = database;
 
   // تابع برای ترسیم باکس و اطلاعات روی Canvas
   const drawBarcodeOverlay = (positions: BarcodePosition[]) => {
