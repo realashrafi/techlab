4f6d10d5bc5afc4108264453ef3e30c845e9d542
ali <zboom0ne01@gmail.com>
2025-08-13 11:46:40 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Main from "./components/Main";
 import Stream from "./components/Stream";
+import BarcodeScanner from "./components/BarcodeScanner";
 
 function App() {
   return (
       <Router>
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/br" element={<BarcodeScanner />} />
           <Route path="/stream" element={<Stream />} />
         </Routes>
       </Router>
